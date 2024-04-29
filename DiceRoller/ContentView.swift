42fec6d478d1cb2303a370cfc80d68f3ad149395
SaraHematian <sara.hematian78@gmail.com>
2024-04-29 14:31:57 -0700

    final

---
+++ DiceRoller/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Armina on 4/29/24.
+//
+
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+    
+    var body: some View {
+        VStack {
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+                .foregroundStyle(.white)
+            
+            HStack {
+                ForEach(1...numberOfDice, id: \.self) { _ in
+                    DiceView()
+                        .foregroundStyle(.white)
+                }
+            }
+            
+            HStack {
+                Button("Remove Dice", systemImage: "minus.circle.fill") {
+                    numberOfDice -= 1
+                }
+                .disabled(numberOfDice == 1)
+                
+                Button("Add Dice", systemImage: "plus.circle.fill") {
+                    numberOfDice += 1
+                }
+                .disabled(numberOfDice == 5)
+            }
+            .padding()
+            .labelStyle(.iconOnly)
+            .font(.title)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.blue)
+        .tint(.white)
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ DiceRoller/DiceView.swift
@@ -0,0 +1,38 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Armina on 4/29/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct DiceView: View {
+    @State private var numberOfPips: Int = 1
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips).fill")
+                .resizable()
+                .frame(maxWidth: 100, maxHeight: 100)
+                .aspectRatio(1, contentMode: .fit)
+                .foregroundStyle(.black, .white)
+            
+            Button("Roll") {
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+}
+
+
+#Preview {
+    DiceView()
+}
+
+
